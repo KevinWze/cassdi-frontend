2d796ea879d0f09b6832e25d3eb0fe98aa9a9f31
KevinWze <kevin_1321124014@uptecamac.edu.mx>
2024-08-04 23:54:35 -0600

    legislacionjs

---
+++ pages/legislacion.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FeatureComponent from "../components/custom/sections/FeatureComponent"; // Asegúrate de que la ruta sea correcta
+
+const LegislacionPage = () => {
+  return (
+    <div>
+      <FeatureComponent />
+    </div>
+  );
+};
+
+export default LegislacionPage;
